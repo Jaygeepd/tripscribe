5e4f367bbb6eb370daa7514d8a02ce790ad53b76
John Mahon <jgmahon13@gmail.com>
2022-10-28 17:43:58 +0100

    Filled out the flyway files for the database automation

--- migrations/base/V1.1__data_seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO location(location_type) VALUES ('Restaurant');
-INSERT INTO location(location_type) VALUES ('Attraction');
-INSERT INTO location(location_type) VALUES ('Scenic Spot');
-INSERT INTO location(location_type) VALUES ('Other');
